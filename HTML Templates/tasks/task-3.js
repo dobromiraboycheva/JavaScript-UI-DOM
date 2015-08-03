41fd1a0a974a5ae24ba7c19736bc5da6821d6e10
dobomiraboycheva <dobromira.boycheva@gmail.com>
2015-08-03 11:21:40 +0300

    HW

---
+++ HTML Templates/tasks/task-3.js
@@ -0,0 +1,19 @@
+function solve(){
+  return function(){
+    $.fn.listview = function(data){
+        var $this = $(this),
+            templateSelector = '#' + $this.attr('data-template'),
+            templateHtml = $(templateSelector).html(),
+            template = handlebars.compile(templateHtml),
+            i;
+
+        for (i = 0; i < data.length; i++) {
+            $this.append(template(data[i]));
+        }
+
+        return this;
+    };
+  };
+}
+
+module.exports = solve;
